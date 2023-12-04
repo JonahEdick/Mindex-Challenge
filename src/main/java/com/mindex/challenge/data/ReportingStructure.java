a1a2cc99bd9af598da0c88b881640058bcfcf458
JonahEdick <jonah.edick@gmail.com>
2023-12-04 17:47:45 -0500

    Relavent comments added and Reporting Structure equals override implemented

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -8,32 +8,31 @@ import com.mindex.challenge.dao.EmployeeRepository;
 
 public class ReportingStructure {
     private Employee employee;
+    
+    // Default is 0 for an employee with no direct reports
     private int numberOfReports = 0;
 
-    /**
-     * 
-     */
-    public ReportingStructure(){
+    public ReportingStructure(){}
 
-    }
-
-    /**
-     * 
-     * @param employee
-     */
     public ReportingStructure(Employee employee, EmployeeRepository employeeRepository){
         this.employee = employee;
 
+        // List of all relavent reports
         List<Employee> directReports = this.employee.getDirectReports();
 
         if(directReports != null){
+            // Create set to store total reports
             Set<Employee> reportSet = new HashSet<>();
             Employee tempReport;
             List<Employee> subReports;
+
+            // Iterate through all direct reports
             for (Employee directReport : directReports) {
                 tempReport = employeeRepository.findByEmployeeId(directReport.getEmployeeId());
                 reportSet.add(tempReport);
                 subReports = tempReport.getDirectReports();
+
+                // Iterate through potential distinct reports
                 if(subReports != null)
                     for(Employee subReport : subReports){
                         reportSet.add(employeeRepository.findByEmployeeId(subReport.getEmployeeId()));
@@ -43,19 +42,27 @@ public class ReportingStructure {
         }        
     }
 
-    /**
-     * 
-     * @return
-     */
     public int getNumberOfReports(){
         return numberOfReports;
     }
 
-    /**
-     * 
-     * @return
-     */
     public Employee getEmployee(){
         return employee;
+    }
+
+    @Override
+    public boolean equals(Object test){
+        // Checks to see if they are the same instantiated object.
+        if(test == this) return true;
+
+        // Checks to see if the tested object is the same class.
+        if(!(test instanceof ReportingStructure)) return false;
+
+        ReportingStructure testStructure = (ReportingStructure) test;
+
+        return
+            this.employee.equals(testStructure.getEmployee()) &&
+            this.numberOfReports == testStructure.getNumberOfReports()
+        ;
     }
 }
--- src/test/java/com/mindex/challenge/service/impl/ReportingStructureImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/ReportingStructureImplTest.java
@@ -6,8 +6,6 @@ import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,8 +34,10 @@ public class ReportingStructureImplTest {
 
     @Before
     public void setup(){
+        // Set up consistent endpoint for testing
         reportingUrl = "http://localhost:" + port + "/reporting/{id}";
 
+        // Set up an array of tests for each member in test data
         emplpoyeeTests = new Hashtable<>();
         emplpoyeeTests.put("16a596ae-edd3-4847-99fe-c4518e82c86f", 4);
         emplpoyeeTests.put("b7839309-3348-463b-a7e3-5de1c168beb3", 0);
@@ -47,18 +47,27 @@ public class ReportingStructureImplTest {
     }
 
     @Test
+    /**
+     * Test for when nothing is passed in as a parameter
+     */
     public void testNullCall(){
         HttpStatus actual = restTemplate.getForEntity(reportingUrl, Exception.class, "").getStatusCode();
         assertErrorResponse(HttpStatus.NOT_FOUND, actual);
     }
 
     @Test
+    /**
+     * Test for when nothing is 
+     */
     public void testInvalidId(){
         HttpStatus actual = restTemplate.getForEntity(reportingUrl, Exception.class, "intentional-failure").getStatusCode();
         assertErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, actual);
     }
 
     @Test
+    /**
+     * Iteratively test valid reports from test dictionary
+     */
     public void testValidReports(){
         ReportingStructure result;
 
